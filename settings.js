c35be9b0001bb555f2d137e8b9209c2d0e161f60
bilal2819 <93931532+bilal2819@users.noreply.github.com>
2022-08-16 11:10:02 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -69,8 +69,8 @@ global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "hi" //ur sticker watermark packname
+global.author = "bye" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
